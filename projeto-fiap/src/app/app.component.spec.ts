04376547d7bdcc2af21bee3c3c8ac3f70532747c
Eduardo José <eduard.jcc@gmail.com>
2022-12-14 10:09:55 -0300

    task - ajuste.

--- projeto-fiap/src/app/app.component.spec.ts
+++ projeto-fiap/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'projeto-fiap'`, () => {
+  it(`should have as title 'youtube'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('projeto-fiap');
+    expect(app.title).toEqual('youtube');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('projeto-fiap app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('youtube app is running!');
   });
 });
